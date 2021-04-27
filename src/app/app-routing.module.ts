5d9adfeeeec6b6a6b4d1e5006bb0aaed7bd672e7
deepikaakshaya <deepikavijayakumar1998@gmail.com>
2021-04-27 20:33:10 +0530

    changes in app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BuynsellComponent } from './buynsell/buynsell.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { IpoComponent } from './ipo/ipo.component';
 import { LoginComponent } from './login/login.component';
 import { ProductShopComponent } from './product-shop/product-shop.component';
 import { RegisterComponent } from './register/register.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'buysell', component: BuynsellComponent },
   { path: 'shop', component: ProductShopComponent },
   { path: 'home', component: HomepageComponent },
+  { path: 'ipo', component: IpoComponent },
 ];
 
 @NgModule({
