04c96aee34a6c387360a4f5318474f7c3f3dc482
deepikaakshaya <deepikavijayakumar1998@gmail.com>
2021-05-25 15:35:21 +0530

    saved

--- src/app/ipo/ipo.component.ts
+++ src/app/ipo/ipo.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-ipo',
   templateUrl: './ipo.component.html',
   styleUrls: ['./ipo.component.css']
 })
 export class IpoComponent implements OnInit {
-status="";
-hide=true;
-  constructor() { 
+
+  state="";
+  registrationForm :FormGroup;
+
+  constructor(private builder: FormBuilder) { }
+  ngOnInit() {
+    this.registrationForm = this.builder.group({
+      'cpyname': new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50)
+    ]),
+        'mvalue': new FormControl('',
+         Validators.required),
+         'per': new FormControl('',
+         Validators.required),           
+    })
   }
-  ngOnInit(): void {
+
+Submit(){
+  if(this.registrationForm.status==="VALID")
+  {
+    this.state="Successfully registered!!! IPO services initiated, you will be informed once IPO quote is prepared";
   }
-createserver()
-{
-  this.status="Successfully registered!!! IPO services initiated, you will be informed once IPO quote is prepared";
-  this.hide=false;
+  else if(this.registrationForm.status==="INVALID")
+  {
+    this.state="System currently unavailable contact our banking representative to initiate the process";
+  }
 }
-registrationForm = new FormGroup({
-  'cpyname': new FormControl('', [
-    Validators.required,
-    Validators.minLength(2),
-    Validators.maxLength(50)
-]),
-    'mvalue': new FormControl('',
-     Validators.required),
-     'per': new FormControl('',
-     Validators.required),
-     'but': new FormControl('',Validators.compose([
-       Validators.required])
-       )
-})
 
 }
