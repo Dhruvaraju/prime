52cb0eef3a7d2e33b1e35cbf2f32e49edd4cb7fe
angeleenajohn <angeleenajohn97@gmail.com>
2021-05-28 18:17:16 +0530

    feat(buy & sell): Added functions

--- src/app/buynsell/buy/buy.component.ts
+++ src/app/buynsell/buy/buy.component.ts
@@ -5,14 +5,13 @@ import { StocksService } from './../../services/stocks.service';
 @Component({
   selector: 'app-buy',
   templateUrl: './buy.component.html',
-  styleUrls: ['./buy.component.css']
+  styleUrls: ['./buy.component.css'],
 })
 export class BuyComponent implements OnInit {
-  
-  submitted = false;
   public selectStocks = [];
   enableBillPrice: boolean = false;
-  buyForm: FormGroup;  
+  priceErrorBanner: boolean = false; //Price Error Display Banner
+  buyForm: FormGroup;
 
   get stocks() {
     return this.buyForm.get('stocks');
@@ -30,40 +29,46 @@ export class BuyComponent implements OnInit {
     return this.buyForm.get('price');
   }
 
-  constructor( private fb: FormBuilder, 
-               private stock: StocksService ) { }
-
+  constructor( private fb: FormBuilder,
+               private stock: StocksService ) {}
 
   ngOnInit() {
-  this.buyForm = this.fb.group({
-    stocks: ['', Validators.required],
-    quantity: ['', Validators.required],
-    orderType: ['', Validators.required],
-    price: ['', Validators.required]
-  });
+    this.buyForm = this.fb.group({
+      stocks: ["select-stock", Validators.required ],
+      quantity: ['', Validators.required ],
+      orderType: ["Select", Validators.required ],
+      price: [''],
+    });
 
-  this.stock.getStocks()
-    .subscribe(data => this.selectStocks = data);
+    this.stock.getStocks()
+              .subscribe((data) => (this.selectStocks = data));
   }
 
   onOrderTypeChange() {
-    if(this.buyForm.get('orderType').value === "limit") {
+    if (this.buyForm.get('orderType').value === 'limit') {
       this.enableBillPrice = true;
-    }
-    else {
+    } else {
       this.enableBillPrice = false;
     }
   }
 
-  onSubmit() {
-    this.submitted = true;
-    if (this.buyForm.valid) {
-      alert('Product Added to your Portfolio!');
-    }
-    else{
-      alert('Error!Try Again');
-    }
-  //  console.log(this.buyForm);
+  onPriceChange() {
+    this.priceErrorBanner = false;
   }
 
+  onSubmit() {
+    if (
+      this.buyForm.get('orderType').value === 'limit' &&
+      this.buyForm.get('price').value === ''
+    ) {
+      this.priceErrorBanner = true;
+      return null;
+    }
+    if (this.buyForm.valid) {
+      alert('Product Added to your Portfolio!');
+    } else {
+      alert('Error!Try Again');
+    }
+    this.buyForm.reset()
+  }
 }
