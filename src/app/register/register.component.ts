b2f9b0e6381a48cf977eb2351f8bd4407d6000ce
KarthikaNK <kathunk10@gmail.com>
2021-05-31 16:08:45 +0530

    'check'

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,41 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder,Validators} from '@angular/forms';
-import {passwordValidator} from '../password.validator';
+import { FormBuilder, Validators } from '@angular/forms';
+import { passwordValidator } from '../password.validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-   submitted=false;
-   constructor(private fb:FormBuilder) { }
-   registrationForm=this.fb.group({
-    firstname:['',Validators.required],
-    lastname:['',Validators.required],
-    phone:['',[Validators.required,Validators.maxLength(10)]],
-    email:['',Validators.required],
-    pan:['',[Validators.required,Validators.maxLength(10)]],
-    address01:['',Validators.required],
-    address02:['',Validators.required],
-    state:['',Validators.required],
-    country:['',Validators.required],
-    postalcode:['',Validators.required],
-    username:['',[Validators.required,Validators.minLength(6)]],
-    password:['',Validators.required],
-    confirmpassword:['',Validators.required], 
-    type:['',Validators.required],
-    organisation:['None']
+  submitted = false;
+  constructor(private fb: FormBuilder) {}
+  registrationForm = this.fb.group(
+    {
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.maxLength(10)]],
+      email: ['', Validators.required],
+      pan: ['', [Validators.required, Validators.maxLength(10)]],
+      address01: ['', Validators.required],
+      address02: ['', Validators.required],
+      state: ['', Validators.required],
+      country: ['', Validators.required],
+      postalcode: ['', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required],
+      type: ['', Validators.required],
+      organisation: ['None'],
+    },
+    { validator: passwordValidator }
+  );
 
-
-   },{validator:passwordValidator});
-
-   onsubmit(){
-    this.submitted=true;
-    
-   }
-
-  ngOnInit(): void {
+  onsubmit() {
+    this.submitted = true;
   }
 
+  ngOnInit(): void {}
 }
