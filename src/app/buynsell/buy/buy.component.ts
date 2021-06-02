ec994f7446b6c93df297d6ecaa6923fa9629f05b
angeleenajohn <angeleenajohn97@gmail.com>
2021-06-02 22:10:06 +0530

    Formatted files

--- src/app/buynsell/buy/buy.component.ts
+++ src/app/buynsell/buy/buy.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { StocksService } from '../../services/stocks/stocks.service';
+import { StocksService } from './../../services/stocks/stocks.service';
 
 @Component({
   selector: 'app-buy',
