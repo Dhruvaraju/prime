a52ceaa6d931cc34e62db9e3b1b9e3be04b3be94
angeleenajohn <angeleenajohn97@gmail.com>
2021-06-12 15:12:27 +0530

    feat(Upgrade): Angular 11

--- src/app/buynsell/sell/sell.component.spec.ts
+++ src/app/buynsell/sell/sell.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SellComponent } from './sell.component';
 
@@ -6,7 +6,7 @@ describe('SellComponent', () => {
   let component: SellComponent;
   let fixture: ComponentFixture<SellComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SellComponent],
     }).compileComponents();
--- src/app/income/income.component.spec.ts
+++ src/app/income/income.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { IncomeComponent } from './income.component';
 
@@ -6,7 +6,7 @@ describe('IncomeComponent', () => {
   let component: IncomeComponent;
   let fixture: ComponentFixture<IncomeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ IncomeComponent ]
     })
