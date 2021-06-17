a2f466b385a78a7299e9b82b682135c2cd060d24
Dhruva Raju <49dhruva@gmail.com>
2021-06-17 20:47:37 +0530

    refactor(fpshop): updated wealth and income products page

--- src/app/wealth/wealth.component.ts
+++ src/app/wealth/wealth.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ServiceshopService } from '../services/serviceshop.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ServiceShopService } from '../services/serviceShop.service';
 
 @Component({
   selector: 'app-wealth',
@@ -8,33 +8,76 @@ import { ServiceshopService } from '../services/serviceshop.service';
   styleUrls: ['./wealth.component.css'],
 })
 export class WealthComponent implements OnInit {
-  productAdded = false;
-  errors = false;
   userName: string = localStorage.getItem('username');
+  productList: any;
+  selectedProduct: any;
+  wealthForm: FormGroup;
+  serviceFailureMessage: boolean = false;
+  productOwnedMessage: boolean;
 
-  constructor(private fg: FormBuilder, private ck: ServiceshopService) {}
-  wealthForm = this.fg.group({
-    name: ['', Validators.required],
-    type: [''],
-    value: [''],
-  });
+  constructor(
+    private formBuilder: FormBuilder,
+    private fpService: ServiceShopService
+  ) {
+    this.buildWealthForm();
+  }
 
-  onSubmitWealth() {
-    let pdtName = this.wealthForm.get('name').value;
-    let pdtID = 'WM001';
-    if (pdtName === 'longterm') {
-      pdtID = 'WM002';
-    }
-
-    let wealthdata = {
-      productID: pdtID,
-      userName: this.userName,
-    };
-
-    this.ck.wealth(wealthdata).subscribe((response) => {
-      console.log('Success!', response);
+  /**
+   * Creating form Components
+   */
+  buildWealthForm() {
+    this.wealthForm = this.formBuilder.group({
+      product: this.formBuilder.control('', Validators.required),
     });
   }
 
-  ngOnInit(): void {}
+  /**
+   * Making initial call to get list of product details.
+   * Filtering only Wealth products.
+   */
+  ngOnInit(): void {
+    this.fpService.fetchFinancialProductList().subscribe((res) => {
+      this.productList = res.filter(
+        (product) => product.subcategory === 'WEALTH'
+      );
+    });
+  }
+
+  /**
+   * On clicking submit button this function will be triggered.
+   * Request body is populated, buy service is invoked.
+   * if product is owned, productOwnedMessage is set to true.
+   * Else a success message is created.
+   */
+  onWealthFormSubmit() {
+    this.serviceFailureMessage = false;
+    let selectedProductData = {
+      productID: this.selectedProduct.productID,
+      userName: this.userName,
+    };
+    this.fpService.buyFinancialProduct(selectedProductData).subscribe(
+      (res) => {
+        if (res.message === 'User already owned the product') {
+          this.productOwnedMessage = true;
+        } else {
+          this.wealthForm.reset();
+          this.selectedProduct = null;
+          document.getElementById('successMessage').click();
+        }
+      },
+      (err) => {
+        this.serviceFailureMessage = true;
+      }
+    );
+  }
+
+  /**
+   * On selecting a product from drop down its attribute values are populated.
+   */
+  onProductSelection() {
+    this.productOwnedMessage = false;
+    this.selectedProduct = this.productList.filter(
+      (product) => product.productID === this.wealthForm.get('product').value
+    )[0];
+  }
 }
